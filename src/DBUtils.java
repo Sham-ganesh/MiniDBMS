297791acf4a1df7e446241d8fe4122916c57c057
Sham Ganesh <mohansham.15@gmail.com>
2022-01-05 18:15:35 +0530

    Initial commit

---
+++ src/DBUtils.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.HashMap;
+
+public class DBUtils {
+    public static final String DBFolderPath = "./MiniDB";
+    public static final String DBDetailsFilePath = DBFolderPath + "/DBDetails.txt";
+
+    public static void createDB(String name)  {
+        File newDB = new File(DBFolderPath+"/"+name);
+        if(!newDB.exists()) {
+            newDB.mkdirs();
+        }
+
+        File DBDetails = new File(DBDetailsFilePath);
+        if(!DBDetails.exists()) {
+            try {
+                DBDetails.createNewFile();
+            }catch(IOException e){
+                System.out.println(e);
+            }
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(DBDetailsFilePath, true));
+            writer.write(name+"\n");
+            writer.close();
+        }catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public static void checkForDataBases(HashMap<String, DataBase> DataBases) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(DBDetailsFilePath));
+            String str;
+            while((str = reader.readLine()) != null) {
+                DataBases.put(str, new DataBase(str,DBFolderPath+"/"+str));
+            }
+        }catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/DataBase.java
@@ -0,0 +1,18 @@
+public class DataBase {
+    public String DBName;
+    public String filePath;
+
+    DataBase(String DBName, String filePath) {
+        this.DBName = DBName;
+        this.filePath = filePath;
+    }
+    public void createTable() {
+
+    }
+    public void dropTable() {
+
+    }
+    public void generateReport() {
+
+    }
+}
