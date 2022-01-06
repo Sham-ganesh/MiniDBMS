5ba0c8d8fe274ba6352cc8220c758f9df72b92c5
yogeeswar2001 <yogeeswar9656vm@gmail.com>
2022-01-06 10:53:52 +0530

    added code for DataBase class

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DataBase {
     public String DBName;
     public String filePath;
@@ -7,10 +12,37 @@ public class DataBase {
         this.filePath = filePath;
     }
     public void createTable(String tableName, String[] colNames, String[] colTypes) {
-
+        File table = new File(filePath+"/"+tableName+".txt");
+        if( table.exists() ) {
+            System.out.println("Table already exists");
+            return;
+        }
+        else {
+            try {
+                table.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath+"/"+tableName+".txt", true));
+            for(int i=0;i<colNames.length;i++) {
+                writer.write(colNames[i]+" ");
+            }
+            writer.write("\n");
+            for(int i=0;i<colTypes.length;i++) {
+                writer.write(colTypes[i]+" ");
+            }
+            writer.write("\n");
+        }catch (IOException e ) {
+            System.out.println(e);
+        }
     }
     public void dropTable(String tableName) {
-
+        File table = new File(filePath+"/"+tableName+".txt");
+        if(table.delete()) {
+            System.out.println("Table " + tableName + "Dropped successfully");
+        }
     }
     public void generateReport() {
 
