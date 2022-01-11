bd578c43b54351734cd988e34554396f9a3aeda2
Sham Ganesh <mohansham.15@gmail.com>
2022-01-11 21:33:59 +0530

    Login and Signup Completed

--- src/main/java/loginServlet.java
+++ src/main/java/loginServlet.java
@@ -3,6 +3,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.*;
 
@@ -12,16 +14,41 @@ public class loginServlet extends HttpServlet {
             String userName = req.getParameter("uname");
             String pwd = req.getParameter("pass");
 
-            Main user = new Main();
+            BufferedReader in = new BufferedReader(new FileReader("users.txt"));
+            String line;int flag=0;
+            String[] str=new String[20];
+            while((line=in.readLine())!=null)
+            {
+                str=line.split(",");
+                if(str[0].equals(userName))
+                {
+                    if(str[1].equals(pwd)) {
+                        in.close();
+                        Main user = new Main();
+                        ArrayList<String> names = new ArrayList<>();
+                        for(String name : user.DataBases.keySet())
+                            names.add(name);
 
-            ArrayList<String> names = new ArrayList<>();
-            for(String name : user.DataBases.keySet())
-                names.add(name);
-
-            req.setAttribute("DBNames", names);
-            RequestDispatcher view = req.getRequestDispatcher("database.jsp");
-            view.forward(req, res);
-
+                        req.setAttribute("DBNames", names);
+                        RequestDispatcher view = req.getRequestDispatcher("database.jsp");
+                    }
+                    else{
+                        PrintWriter out = res.getWriter();
+                        out.println("<script type=\"text/javascript\">");
+                        out.println("alert('Password incorrect- never ever try to guess it!!');");
+                        out.println("location='index.html';");
+                        out.println("</script>");
+                    }
+                    flag=1;
+                }
+            }
+            if(flag==0) {
+                PrintWriter out = res.getWriter();
+                out.println("<script type=\"text/javascript\">");
+                out.println("alert('User Not Found - Check your name.');");
+                out.println("location='index.html';");
+                out.println("</script>");
+            }
         }catch (Exception e) {
             System.out.println(e);
         }
